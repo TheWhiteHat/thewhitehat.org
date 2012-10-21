be8af035b5d36b7eaf0e5b7cc1aee69d0078f23c
Jared Shumway <jaredshumway94@gmail.com>
2012-10-20 23:48:30 -0700

    Fixed some blog templates

--- blog/views.py
+++ blog/views.py
@@ -12,30 +12,13 @@ def index(request, page=1):
 
 # view the detail(s) of single entry
 def entry_detail(request, slug):
-    entries = Entry.objects.filter(slug=slug)
+    entry = Entry.objects.get(slug=slug)
     
-    return render_to_response('blog/entry_detail.html', {'entries':entries})
+    return render_to_response('blog/entry_detail.html', {'entry':entry})
 
 # view a list of all entries in time order potentially filtered by
 # author, tag, or category
 def entry_list(request):
     entries = Entry.objects.order_by('-date_created')
 
-    ## Sort the entries into groups by month. Probably a better way to
-    ## do this
-
-    final = []
-    temp = []
-    curr = entries[0].date_created.month
-
-    for q in entries:
-        d = q.date_created.month
-        if d < curr:
-            curr = d
-            final.append(temp)
-            temp = []
-        temp.append(q)
-
-    final.append(temp)
-
-    return render_to_response('blog/entry_list.html', {'entries':final, 'list_title':'Every Entry'})
+    return render_to_response('blog/entry_list.html', {'entries':entries, 'list_title':'Every Entry'})
