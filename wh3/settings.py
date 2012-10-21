40f705273c5c8fb206943f605040c01feab6b014
ThePicard <pfaiman@calpoly.edu>
2012-10-20 17:00:13 -0700

    base template, proj_rel, assets/templates/static
    proj_rel will automatically find the project root and create a path
    relative to the project root. this allows us to share the same config
    for all people's dev machines and heroku
    
    also, assets is where static assets will go (css/js/etc), and they will
    automatically be added into the static files directory, along with any
    static assets found by the app loader

--- wh3/settings.py
+++ wh3/settings.py
@@ -1,6 +1,11 @@
 # Django settings for wh3 project.
 import dj_database_url
 
+import os.path
+PROJECT_ROOT = os.path.realpath(os.path.join(os.path.dirname(os.path.normpath(__file__)), '..')) # wh3/..
+def proj_rel(path):
+    return os.path.realpath(os.path.join(PROJECT_ROOT, path)) # PROJECT_ROOT/path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -22,27 +27,12 @@ DATABASES = {
     #}
 }
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Los_Angeles'
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
-
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
+USE_I18N = False # not needed
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
@@ -54,25 +44,12 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
+STATIC_ROOT = proj_rel('static/')
 STATIC_URL = '/static/'
-
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    proj_rel('assets'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
@@ -82,7 +59,10 @@ STATICFILES_FINDERS = (
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'fflu@&amp;_-6ag=5j$c3!vq2#8&amp;8kz5j1wzq*814x0^a-40x1#z9u'
 
-# List of callables that know how to import templates from various sources.
+TEMPLATE_DIRS = (
+    proj_rel('templates/'),
+)
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -100,15 +80,7 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = 'wh3.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wh3.wsgi.application'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
