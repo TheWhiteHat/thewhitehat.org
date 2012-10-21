a5b85099483bb77c3eb60595903109ab4cd195a8
Jared Shumway <jaredshumway94@gmail.com>
2012-10-20 18:45:48 -0700

    Fixed the entry_detail page

--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,8 @@ def index(request, page=1):
 # view a single entry
 def entry_detail(request, slug):
     entry = Entry.objects.get(slug=slug)
-    entry.date = entry.date_modified
     
-    return render_to_response('blog/entry.html', {'entry':entry})
+    return render_to_response('blog/entry_detail.html', {'entry':entry})
 
 # view a list of all entries in time order potentially filtered by
 # author, tag, or category
