04a75dd57c676a6e2b3ced1903b018c269694ab0
ThePicard <pfaiman@calpoly.edu>
2012-10-20 17:15:00 -0700

    being anal about the database

--- blog/models.py
+++ blog/models.py
@@ -3,24 +3,21 @@ from django.contrib.auth.models import User
 
 # an over-arching category for entries. one cat. per entry.
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField()
-    slug = models.SlugField(max_length=50)
-
+    name = models.SlugField()
+    description = models.CharField(max_length=126)
 
 # a more specific categorization for entries. multiple tags per entry.
 class Tag(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.SlugField()
 
-
-# an entry
+# a blog entry
 class Entry(models.Model):  
-    headline = models.CharField(max_length=100)
+    headline = models.CharField(max_length=126)
+    author = models.ForeignKey(User, db_index=True)
+    slug = models.SlugField()
+    tags = models.ManyToManyField(Tag, related_name="entries", db_index=True)
+    category = models.ForeignKey(Category, related_name="entries", db_index=True)
     content = models.TextField()
-    date_posted = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     date_modifed = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User)
-    comments_enabled = models.BooleanField(default=True)
-    slug = models.SlugField(max_length=50)
-    tags = models.ManyToManyField(Tag)
-    category = models.ForeignKey(Category)
+    enable_comments = models.BooleanField(default=True)
