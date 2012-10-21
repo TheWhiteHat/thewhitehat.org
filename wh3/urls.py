a8d824b0fd38ff0599a8441b43a76705d1300932
ThePicard <pfaiman@calpoly.edu>
2012-10-20 19:46:16 -0700

    actual base.html stuff and an index page

--- wh3/urls.py
+++ wh3/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -6,6 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
