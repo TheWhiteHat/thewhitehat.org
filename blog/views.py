0fa284cdab28328b3ffb67f4f1948d4b7a8ad56f
Jared Shumway <jaredshumway94@gmail.com>
2012-10-20 19:43:28 -0700

    Fixed blog pages to correctly display author information, and the blog index to display the five newest posts

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,11 @@ from django.template import RequestContext
 from blog.models import Entry
 from datetime import datetime
 
-# all of the blog posts in order
+# the top 5 blog posts in order from newest to oldest
 def index(request, page=1):
-    entries = Entry.objects.order_by('date_created')[:5]
+    entries = Entry.objects.order_by('date_created')[::-1]
 
-    return render_to_response('blog/index.html', {'entries':entries})
+    return render_to_response('blog/index.html', {'entries':entries[:5]})
 
 # view the detail(s) of single entry
 def entry_detail(request, slug):
