9b5aacd2fa547af45af4453a5c42f3efe172c195
ThePicard <pfaiman@calpoly.edu>
2012-10-25 11:12:49 -0700

    added logo image and made index show blog posts

--- wh3/urls.py
+++ wh3/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from blog.models import Entry
 
 from django.contrib import admin
 admin.autodiscover()
 
-
 urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')),
     url(r'^contact/', include('contact.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^$', ListView.as_view(template_name='index.html', context_object_name='entries', queryset=Entry.objects.order_by('-date_created')[:5]), name='index'),
     url(r'^gallery/?', TemplateView.as_view(template_name='single_pages/gallery.html'), name='gallery'),
     url(r'^admin/', include(admin.site.urls)),
 
