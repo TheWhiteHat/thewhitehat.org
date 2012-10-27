4f98c42453ff8f77401be0d15070d8837012d2f3
Jared Shumway <jared@ackbar>
2012-10-26 23:47:49 -0700

    better blag index, list
    Added a sidebar to the blog index. Changed the layout of the entry_list.

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import RequestContext
-from blog.models import Entry
+from blog.models import Entry, Tag, Category
 from datetime import datetime
 
 # the top 5 blog posts in order from newest to oldest
 def index(request, page=1):
     entries = Entry.objects.order_by('-date_created').select_related()[:5]
 
-    return render(request, 'blog/index.html', {'entries':entries})
+    return render(request, 'blog/index.html', {'entries':entries, 'categories':Category.objects.all(), 'tags':Tag.objects.all()})
 
 # view the detail(s) of single entry
 def entry_detail(request, slug):
@@ -23,15 +23,18 @@ def entry_list(request, **kwargs):
     if 'author' in kwargs:
         entries = entries.filter(author__username=kwargs['author'])
         title = kwargs['author']
+        limitby = 'by ' + kwargs['author']
     elif 'category' in kwargs:
         entries = entries.filter(category__name=kwargs['category'])
         title = kwargs['category'] + ' category'
+        limitby = 'in the category ' + kwargs['category']
     elif 'tag' in kwargs:
         entries = entries.filter(tags__name=kwargs['tag'])
         title = kwargs['tag'] + ' tagged'
+        limitby = 'tagged ' + kwargs['tag']
     else:
         title = 'Archive'
     
     # user entry_list2.html for verybueno's listing technique.
-    return render(request, 'blog/entry_list.html', {'entries':entries, 'title':title})
+    return render(request, 'blog/entry_list.html', {'entries':entries, 'title':title, 'limitby':limitby})
 
