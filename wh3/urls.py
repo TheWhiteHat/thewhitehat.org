570eb9f7d963246ad466d390886a0258e0fb8e5a
Jared Shumway <jared@ackbar>
2012-11-04 01:43:17 -0800

    new index page

--- wh3/urls.py
+++ wh3/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^blog/', include('blog.urls')),
     url(r'^contact/', include('contact.urls')),
-    url(r'^$', ListView.as_view(template_name='index.html', context_object_name='entries', queryset=Entry.objects.order_by('-date_created')[:5]), name='index'),
+    url(r'^$', ListView.as_view(template_name='index.html', context_object_name='entries', queryset=Entry.objects.order_by('-date_created')[:6]), name='index'),
     url(r'^gallery/?', TemplateView.as_view(template_name='single_pages/gallery.html'), name='gallery'),
     url(r'^admin/', include(admin.site.urls)),
 
