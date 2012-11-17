9dbb269eb1e54815b23346413962e34c7c7ea116
ThePicard <pfaiman@calpoly.edu>
2012-11-17 05:46:56 -0800

    vacuum WOOOOOOOOSSSHHHH

--- contact/views.py
+++ contact/views.py
@@ -1,19 +1,16 @@
-from django.http import HttpResponse, Http404,HttpResponseRedirect
-from django.core.context_processors import csrf
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.http import HttpResponseRedirect
 from contact.models import *
 
 from django.core.urlresolvers import reverse
 
+
 def contact(request):
-  if request.method == 'POST':
-    form = ContactForm(request.POST)
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
     if form.is_valid():
         # send mail lol
-      return HttpResponseRedirect(reverse('contact_success')) # Redirect after POST
-  else:
-    form = ContactForm() # An unbound form
+        return HttpResponseRedirect(reverse('contact_success'))  # Redirect after POST
+    else:
+        form = ContactForm()  # An unbound form
 
-  return render_to_response('contact/contact.html', {'form':form},context_instance=RequestContext(request))
-
+    return render(request, 'contact/contact.html', {'form': form})
