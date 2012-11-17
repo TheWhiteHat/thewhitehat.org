ea76749ef0de07356948eba853972ca64c1a988c
ThePicard <pfaiman@calpoly.edu>
2012-11-17 06:00:01 -0800

    bored this morning, pep8 commit

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 
 from blog import models
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
 admin.site.register(models.Category, CategoryAdmin)
 
+
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
 admin.site.register(models.Tag, TagAdmin)
+
 
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('headline', 'author', 'category', 'date_created')
